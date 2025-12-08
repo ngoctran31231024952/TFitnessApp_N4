3b994337b2a8b45ffb3f489e0fc43084ceec3a60
ngoctran31231024952 <ngoctran.31231024952@ueh.edu.vn>
2025-12-08 11:55:40 +0700

    VietHoaTenHam(2)

--- TFitnessApp/Database/TruyCapDB.cs
+++ TFitnessApp/Database/TruyCapDB.cs
@@ -4,11 +4,11 @@ using Microsoft.Data.Sqlite;
 
 namespace TFitnessApp.Database
 {
-    public class DbAccess
+    public class TruyCapDB
     {
         private static readonly string ChuoiKetNoi;
 
-        static DbAccess()
+        static TruyCapDB()
         {
             string duongDanDB = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Database", "Tfitness.db");
             ChuoiKetNoi = $"Data Source={duongDanDB};";
@@ -16,7 +16,7 @@ namespace TFitnessApp.Database
         // Thuộc tính để truy cập chuỗi kết nối
         public string _ChuoiKetNoi => ChuoiKetNoi;
         // Phương thức tạo và mở kết nối
-        public static SqliteConnection CreateConnection()
+        public static SqliteConnection TaoKetNoi()
         {
             var conn = new SqliteConnection(ChuoiKetNoi);
             conn.Open();
